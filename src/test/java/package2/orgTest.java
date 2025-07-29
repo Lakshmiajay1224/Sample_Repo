ef9afeb9ebdffb2cbe16e397616785870f7b3703
Lakshmi N <Lakshmi N@Ak.mshome.net>
2025-07-29 15:52:40 +0530

    first push

---
+++ src/test/java/package2/orgTest.java
@@ -0,0 +1,14 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createorgTest() {
+		System.out.println("execute createorgTest");
+	}
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute modifyorgTest");
+	}
+}
