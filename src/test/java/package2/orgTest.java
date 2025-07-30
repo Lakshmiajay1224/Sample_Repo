f01c88c5bc790cad333269424b560288498f20b7
Lakshmi N <Lakshmi N@Ak>
2025-07-30 11:03:19 +0530

    add org test

--- src/test/java/package2/orgTest.java
+++ src/test/java/package2/orgTest.java
@@ -6,6 +6,14 @@ public class orgTest {
 	@Test
 	public void createorgTest() {
 		System.out.println("execute createorgTest");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	@Test
 	public void modifyorgTest() {
